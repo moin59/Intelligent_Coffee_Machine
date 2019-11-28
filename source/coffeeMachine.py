d48320a6c418a708775269657e0dc4fd7725cf19
moin59 <eee.moin@gmail.com>
2019-11-28 16:13:28 +0100

    some information has been changed

--- source/coffeeMachine.py
+++ source/coffeeMachine.py
@@ -1,9 +1,7 @@
 import os
 import time
-
-
 # Defined class for coffee machine
-class Intelligent_CoffeeMachine():
+class intelligent_CoffeeMachine():
 
     # some condition initialized as boolean and set initial value
     def __init__(self):
@@ -134,49 +132,3 @@ class Intelligent_CoffeeMachine():
         print("Machine is Switching OFF!...")
         time.sleep(5)
         print("Machine has been switched OFF!")
-
-
-coffee_machine = Intelligent_CoffeeMachine()  # instance of coffee machine
-
-user_option = " "
-# while loop for user menu, until press 'e' loop will continue
-while user_option != 'e':
-    os.system('cls')
-    print(" =================INTELLIGENT MENU================\n"
-          "| Select <numbers> from the following Options   | \n"
-          "| 1- Open the Lid                               | \n"
-          "| 2-Insert new filter                           | \n"
-          "| 3-Insert coffee (50 grams) per selection      | \n"
-          "| 4-Insert water (200 ml) per selection         | \n"
-          "| 5-Close Lid                                   | \n"
-          "| 6-Start cooking                               | \n"
-          "| 7-Take coffee                                 | \n"
-          "| 8-Switch off the machine                      | \n"
-          "| 9-Check Water and Coffee Reservoir            | \n"
-          "|                  <'e' to EXIT>                | \n"
-          "================================================= \n"
-          "Choose any option by type from 1 to 9 or 'e' for EXIT):")
-
-    user_option = input()
-    # condition of user input. call each object of class"intelligent_CoffeeMachine()" by instance "coffee_machine"
-    if user_option == str(1):
-        coffee_machine.openLid()
-    elif user_option == str(2):
-        coffee_machine.filterInsert()
-    elif user_option == str(3):
-        coffee_machine.insertCoffee(50)
-    elif user_option == str(4):
-        coffee_machine.insertWater(200)
-    elif user_option == str(5):
-        coffee_machine.closeLid()
-    elif user_option == str(6):
-        coffee_machine.startCoocking()
-    elif user_option == str(7):
-        coffee_machine.takeCoffee()
-    elif user_option == str(8):
-        coffee_machine.switchOff()
-    elif user_option == str(9):
-        print("The water remained", coffee_machine.waterReservoir, "ml")
-        print("The coffee remained", coffee_machine.coffeeReservoir, "grams")
-    else:
-        print("Operation Closed!")
